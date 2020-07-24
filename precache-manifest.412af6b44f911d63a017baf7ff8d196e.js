8d9ac19653ecf1993d33d0259fbb34268c1c511c
Duy Bui <48144662+duybui-1@users.noreply.github.com>
2020-07-24 15:17:00 -0500

    Add files via upload
    First Push

---
+++ precache-manifest.412af6b44f911d63a017baf7ff8d196e.js
@@ -0,0 +1,190 @@
+self.__precacheManifest = [
+  {
+    "revision": "19778450f2a8f5ac4f2f",
+    "url": "/static/js/app.763d3511.chunk.js"
+  },
+  {
+    "revision": "e91487e8c8aa62ce91ce",
+    "url": "/static/js/runtime~app.34c76111.js"
+  },
+  {
+    "revision": "65d7bf65562a341bcc6d",
+    "url": "/static/js/2.c6510ee6.chunk.js"
+  },
+  {
+    "revision": "004c2bbb035d8d06bb830efc4673c886",
+    "url": "/static/media/star.004c2bbb.png"
+  },
+  {
+    "revision": "2327736b3ea09c41abfb69be1221f430",
+    "url": "/static/media/heart.2327736b.png"
+  },
+  {
+    "revision": "51671417ef20e0bbc32f0a2bc6edfa95",
+    "url": "/static/media/rocket.51671417.png"
+  },
+  {
+    "revision": "a7b9dc9de5f8f1fb1afbef917619a5ac",
+    "url": "/static/media/bell.a7b9dc9d.png"
+  },
+  {
+    "revision": "5a293a273bee8d740a045d9922b9a9ae",
+    "url": "/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "6beba7e6834963f7f171d3bdd075c915",
+    "url": "/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/./fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "23a4c2deef3a0cc9b40b429ad7320a18",
+    "url": "/./fonts/SourceSansPro_200ExtraLight.ttf"
+  },
+  {
+    "revision": "542b437ac4636f19f2a5475170678e07",
+    "url": "/./fonts/SourceSansPro_200ExtraLight_Italic.ttf"
+  },
+  {
+    "revision": "81cd217e4a8160a930c6d5fb8d1e8e82",
+    "url": "/./fonts/SourceSansPro_300Light.ttf"
+  },
+  {
+    "revision": "54e87dedd7daccc391c61dc0795a97bd",
+    "url": "/./fonts/SourceSansPro_300Light_Italic.ttf"
+  },
+  {
+    "revision": "c1678b46f7dd3f50ceac94ed4e0ad01a",
+    "url": "/./fonts/SourceSansPro_400Regular.ttf"
+  },
+  {
+    "revision": "3d7cb86547ce5075625915f2e86d0687",
+    "url": "/./fonts/SourceSansPro_400Regular_Italic.ttf"
+  },
+  {
+    "revision": "83476a890be79f84e97b792c9c40d743",
+    "url": "/./fonts/SourceSansPro_600SemiBold.ttf"
+  },
+  {
+    "revision": "400001859e5426d443911e7b60009ba5",
+    "url": "/./fonts/SourceSansPro_600SemiBold_Italic.ttf"
+  },
+  {
+    "revision": "8669b8706bbbdd1482e2fccc4ed96850",
+    "url": "/./fonts/SourceSansPro_700Bold.ttf"
+  },
+  {
+    "revision": "38845daef5ac62cb403040d0017fdd0a",
+    "url": "/./fonts/SourceSansPro_700Bold_Italic.ttf"
+  },
+  {
+    "revision": "9ded577f93b24331f4faadfc5f834ae6",
+    "url": "/./fonts/SourceSansPro_900Black.ttf"
+  },
+  {
+    "revision": "9912ce7396adef3e4895d80f1db48c79",
+    "url": "/./fonts/SourceSansPro_900Black_Italic.ttf"
+  },
+  {
+    "revision": "9341552d8cbfd150ac426cbd065c51a0",
+    "url": "/./fonts/Merriweather_300Light.ttf"
+  },
+  {
+    "revision": "4deed44b6913721447f062c9fa8caac6",
+    "url": "/./fonts/Merriweather_300Light_Italic.ttf"
+  },
+  {
+    "revision": "c97a9fc29652bb4afcdac68020e5d0f7",
+    "url": "/./fonts/Merriweather_400Regular.ttf"
+  },
+  {
+    "revision": "498bf4ee4ac9fab22ad9f814839173b2",
+    "url": "/./fonts/Merriweather_400Regular_Italic.ttf"
+  },
+  {
+    "revision": "9ccb885c9cf8e503d557f15e0b2cbf24",
+    "url": "/./fonts/Merriweather_700Bold.ttf"
+  },
+  {
+    "revision": "63e1f06ea6bf31c8f3143c143e7e6c2a",
+    "url": "/./fonts/Merriweather_700Bold_Italic.ttf"
+  },
+  {
+    "revision": "bfc959a1d3a785caff07d85f82f3e513",
+    "url": "/./fonts/Merriweather_900Black.ttf"
+  },
+  {
+    "revision": "2282b0c9c8dea275e854273c968b20d7",
+    "url": "/./fonts/Merriweather_900Black_Italic.ttf"
+  },
+  {
+    "revision": "872545dde71de3842234bf6afe80c4cb",
+    "url": "/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "c6aef942e3668158ec29d4adcb2e768f",
+    "url": "/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "b49ae8ab2dbccb02c4d11caaacf09eab",
+    "url": "/./fonts/Fontisto.ttf"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "cd62223440bad0b877ffb41ca553dbb4",
+    "url": "/favicon-16.png"
+  },
+  {
+    "revision": "5580ba414368a49781b0290dc7880353",
+    "url": "/favicon-32.png"
+  },
+  {
+    "revision": "867e0c7229d9f43284bc8a566724256c",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "0b370c6452ab44cc58e9803fe20caf65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "008f3225dae1c56d80b2542556e012aa",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "04cf1ae10e30155ca207400239ec1cbd",
+    "url": "/register-service-worker.js"
+  }
+];
